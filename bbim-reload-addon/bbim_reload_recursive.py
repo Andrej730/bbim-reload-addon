a34a6a8f7dbddead8b921d0043286231dd882d61
Benoit DAMAY <bdamay@gmail.com>
2024-05-02 20:37:17 +0200

    Update bbim_reload_recursive.py
    Rewrote sub module inspection - still fails on PropertyGroup

--- bbim-reload-addon/bbim_reload_recursive.py
+++ bbim-reload-addon/bbim_reload_recursive.py
@@ -7,30 +7,40 @@ reregister_modules = ["blenderbim.bim.module.demo"]
 
 def reregister_modules_recursive(module_name):
     print('Module ', module_name)
-    try: 
-        module = importlib.import_module(module_name)
-        # retrieving registered classes with inspect
-        classes = [c for c, o in inspect.getmembers(module, inspect.isclass)
-                if hasattr(bpy.types, c)]
-        # reregistering classes        
-        for class_name in classes:
-            class_obj = getattr(module, class_name)
-            bpy.utils.unregister_class(class_obj)
-        importlib.reload(module)
-        for class_name in classes:
-            print('Reregistering Class', class_name)
-            class_obj = getattr(module, class_name)
-            bpy.utils.register_class(class_obj)
+    module = importlib.import_module(module_name)
+    # retrieving registered classes with inspect
+    classes = []
+    for n, c in inspect.getmembers(module, inspect.isclass): 
+        if hasattr(c, 'is_registered') and c.is_registered:
+            print(c, c.is_registered)
+            classes.append(c) 
 
-        # in case there are submodules reregister them  
-        # i use pkgutil cause for some reason inspect module brings also bpy as a sub module 
-        if '__path__' in dir(module): # seems __path__ doesn't exist on last level  
-            modules = [module_name +'.'+ o.name for o in pkgutil.iter_modules(module.__path__)]
-            print('module has summodules:')
-            for module_name in modules:
-                reregister_modules_recursive(module_name)
-    except Exception as e:        
-        print('**some errors have occurred in ', module_name, e)
+    # reregistering classes        
+    for c in classes:            
+        try:
+            print('unregistering Class', c)
+            bpy.utils.unregister_class(c)
+            print('Reregistering Class', c)
+            bpy.utils.register_class(c)
+        except:
+            print('**some errors have occurred in classe ', c, e)
+                
+    importlib.reload(module)
+
+#        if '__path__' in dir(module): # seems __path__ doesn't exist on last level  
+#            modules = [module_name +'.'+ o.name for o in pkgutil.iter_modules(module.__path__)]
+#            print('module has summodules:')
+
+    sub_modules=[]
+    for n, sm in inspect.getmembers(module, inspect.ismodule): 
+        # We should avoid reparsing upper modules here. so check if sub module is a real submodule 
+        if module_name in sm.__name__:                           
+            sub_modules.append(sm.__name__) 
+    for module_name in sub_modules:      
+        print('reregistering sub module ', sm.__name__)      
+        reregister_modules_recursive(module_name)
+            
+
 
 for module_name in reregister_modules:                   
     print('-'*60)
